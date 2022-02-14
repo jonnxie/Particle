1f47719721034eed09a93341408f9f68f0c12793
jonnxie <jonnxie@gmail.com>
2022-02-14 23:37:32 +0800

    Add precompiled strategy

---
+++ Shatter_Object/precompiled.cpp
@@ -0,0 +1,5 @@
+//
+// Created by AnWell on 2022/2/14.
+//
+
+#include "precompiled.h"
---
+++ Shatter_Object/precompiled.h
@@ -0,0 +1,19 @@
+//
+// Created by AnWell on 2022/2/14.
+//
+
+#ifndef PARTICLE_PRECOMPILED_H
+#define PARTICLE_PRECOMPILED_H
+
+#include <iostream>
+#include <memory>
+#include <vector>
+#include <queue>
+#include <thread>
+#include <functional>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <algorithm>
+
+#endif //PARTICLE_PRECOMPILED_H
