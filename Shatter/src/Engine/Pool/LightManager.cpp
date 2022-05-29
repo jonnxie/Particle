7cd0b3c26ce31c707f469bba6168c56bc17d6b92
jonnxie <jonnxie@gmail.com>
2022-05-29 23:15:15 +0800

    A few job about light manager.

--- Shatter/src/Engine/Pool/LightManager.cpp
+++ Shatter/src/Engine/Pool/LightManager.cpp
@@ -23,6 +23,7 @@ LightManager& LightManager::getLight(){
 
 void LightManager::init(size_t _size){
     lights.resize(_size);
+    index = _size;
     initLight();
     size = sizeof(PassLight) * lights.size();
     SingleBPool.createStorageHostBuffer("MultiLight",size,lights.data());
@@ -116,5 +117,75 @@ void LightManager::initLight() {
         light.color = colors[rndCol(rndGen)];
         light.radius = 1.0f + std::abs(rndDist(rndGen));
     }
+}
+
+PassLight &LightManager::operator[](size_t _index) {
+    if (_index >= lights.size())
+    {
+        reallocated();
+    }
+    return lights[_index];
+}
+
+void LightManager::addLight(const PassLight &_light) {
+    if (index >= size) {
+        reallocated();
+    }
+    lights[index] = _light;
+}
+
+void LightManager::reallocated() {
+    lights.resize(lights.size() * 2);
+    size *= 2;
+    SingleBPool.freeBuffer("MultiLight",Buffer_Type::Storage_Buffer);
+    SingleBPool.createStorageBuffer("MultiLight",size,lights.data());
+    (*SingleBPool.getBuffer("MultiLight",Buffer_Type::Storage_Buffer)).map();
+    std::array<VkWriteDescriptorSet,3> mc_write = {};
+    VkDescriptorBufferInfo bufferInfo;
+    bufferInfo.buffer = (*SingleBPool.getBuffer("CameraPos",Buffer_Type::Uniform_Buffer))();
+    bufferInfo.offset = 0;
+    bufferInfo.range = 12;
+    mc_write[0].sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+    mc_write[0].pNext = VK_NULL_HANDLE;
+    mc_write[0].dstSet =  SetPool::getPool()["MultiLight"];
+    mc_write[0].dstBinding = 0;
+    mc_write[0].dstArrayElement = 0;
+    mc_write[0].descriptorCount = 1;
+    mc_write[0].descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+    mc_write[0].pImageInfo = VK_NULL_HANDLE;
+    mc_write[0].pBufferInfo = &bufferInfo;
+
+    bufferInfo.buffer = (*SingleBPool.getBuffer("LightCount",Buffer_Type::Uniform_Buffer))();
+    bufferInfo.offset = 0;
+    bufferInfo.range = 4;
+    mc_write[0].sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+    mc_write[0].pNext = VK_NULL_HANDLE;
+    mc_write[0].dstSet =  SetPool::getPool()["MultiLight"];
+    mc_write[0].dstBinding = 1;
+    mc_write[0].dstArrayElement = 0;
+    mc_write[0].descriptorCount = 1;
+    mc_write[0].descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
+    mc_write[0].pImageInfo = VK_NULL_HANDLE;
+    mc_write[0].pBufferInfo = &bufferInfo;
+
+    bufferInfo.buffer = (*SingleBPool.getBuffer("MultiLight",Buffer_Type::Storage_Buffer))();
+    bufferInfo.offset = 0;
+    bufferInfo.range = sizeof(PassLight) * lights.size();
+
+    mc_write[2].sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
+    mc_write[2].pNext = VK_NULL_HANDLE;
+    mc_write[2].dstSet =  SetPool::getPool()["MultiLight"];
+    mc_write[2].dstBinding = 2;
+    mc_write[2].dstArrayElement = 0;
+    mc_write[2].descriptorCount = 1;
+    mc_write[2].descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
+    mc_write[2].pImageInfo = VK_NULL_HANDLE;
+    mc_write[2].pBufferInfo = &bufferInfo;
+
+    vkUpdateDescriptorSets(Device::getDevice()(),
+                           3,
+                           mc_write.data(),
+                           0,
+                           nullptr);
 }
 
--- Shatter/src/Engine/Pool/LightManager.h
+++ Shatter/src/Engine/Pool/LightManager.h
@@ -18,71 +18,16 @@ class LightManager {
 public:
     static LightManager& getLight();
     DefineUnCopy(LightManager);
-    PassLight& operator[](size_t _index){
-        if(_index >= lights.size())
-        {
-//            throw std::runtime_error("light buffer size is not enough big");
-            lights.resize(lights.size() * 2);
-            size *= 2;
-            SingleBPool.freeBuffer("MultiLight",Buffer_Type::Storage_Buffer);
-            SingleBPool.createStorageBuffer("MultiLight",size,lights.data());
-            (*SingleBPool.getBuffer("MultiLight",Buffer_Type::Storage_Buffer)).map();
-            std::array<VkWriteDescriptorSet,3> mc_write = {};
-            VkDescriptorBufferInfo bufferInfo;
-            bufferInfo.buffer = (*SingleBPool.getBuffer("CameraPos",Buffer_Type::Uniform_Buffer))();
-            bufferInfo.offset = 0;
-            bufferInfo.range = 12;
-            mc_write[0].sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
-            mc_write[0].pNext = VK_NULL_HANDLE;
-            mc_write[0].dstSet =  SetPool::getPool()["MultiLight"];
-            mc_write[0].dstBinding = 0;
-            mc_write[0].dstArrayElement = 0;
-            mc_write[0].descriptorCount = 1;
-            mc_write[0].descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
-            mc_write[0].pImageInfo = VK_NULL_HANDLE;
-            mc_write[0].pBufferInfo = &bufferInfo;
-
-            bufferInfo.buffer = (*SingleBPool.getBuffer("LightCount",Buffer_Type::Uniform_Buffer))();
-            bufferInfo.offset = 0;
-            bufferInfo.range = 4;
-            mc_write[0].sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
-            mc_write[0].pNext = VK_NULL_HANDLE;
-            mc_write[0].dstSet =  SetPool::getPool()["MultiLight"];
-            mc_write[0].dstBinding = 1;
-            mc_write[0].dstArrayElement = 0;
-            mc_write[0].descriptorCount = 1;
-            mc_write[0].descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
-            mc_write[0].pImageInfo = VK_NULL_HANDLE;
-            mc_write[0].pBufferInfo = &bufferInfo;
-
-            bufferInfo.buffer = (*SingleBPool.getBuffer("MultiLight",Buffer_Type::Storage_Buffer))();
-            bufferInfo.offset = 0;
-            bufferInfo.range = sizeof(PassLight) * lights.size();
-
-            mc_write[2].sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET;
-            mc_write[2].pNext = VK_NULL_HANDLE;
-            mc_write[2].dstSet =  SetPool::getPool()["MultiLight"];
-            mc_write[2].dstBinding = 2;
-            mc_write[2].dstArrayElement = 0;
-            mc_write[2].descriptorCount = 1;
-            mc_write[2].descriptorType = VK_DESCRIPTOR_TYPE_STORAGE_BUFFER;
-            mc_write[2].pImageInfo = VK_NULL_HANDLE;
-            mc_write[2].pBufferInfo = &bufferInfo;
-
-            vkUpdateDescriptorSets(Device::getDevice()(),
-                                   3,
-                                   mc_write.data(),
-                                   0,
-                                   nullptr);
-        }
-        return lights[_index];
-    }
+    PassLight& operator[](size_t _index);
     void init(size_t _size);
     void initLight();
+    void addLight(const PassLight& _light);
+    void reallocated();
 private:
     std::vector<PassLight> lights;
     size_t size{};
     bool changed = true;
+    size_t index{0};
 private:
     LightManager() = default;
 };
